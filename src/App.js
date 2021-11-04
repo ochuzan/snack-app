7a01f3594fec64832870d913a7d4261fe03914a2
Oliver Chuzan <ochuzan@gmail.com>
2021-11-04 17:53:03 -0400

    Code along during class

--- src/App.js
+++ src/App.js
@@ -1,9 +1,80 @@
 import React from "react";
 import "./App.css";
+import cookieGrogu from "./images/cookieGrogu.jpeg";
+import happyGrogu from "./images/happyGrogu.jpeg";
 
 class App extends React.Component {
+  constructor() {
+    super()
+    
+    this.state = {
+      snackCount: 0,
+      possibleSnacks: [
+        'ice spider',
+        'frog',
+        'alien frog eggs',
+        'blue cookie',
+        'cup of broth'
+      ],
+      snackIndex: 0
+    }
+  }
+
+  giveSnack = () => {
+    const { possibleSnacks, snackIndex, snackCount } = this.state;
+    const currentSnack = possibleSnacks[snackIndex];
+    if (currentSnack === 'ice spider' || currentSnack === 'alien frog eggs'){
+      alert("We shouldn't let Grogu eat that.. let's find a new snack.");
+    } else {
+      this.setState({
+        snackCount: snackCount + 1
+      })
+    }
+  }
+
+  changeSnack = () => {
+    const { possibleSnacks, snackIndex } = this.state;
+    const nextIndex = (snackIndex + 1) % possibleSnacks.length;
+
+    this.setState({
+      snackIndex: nextIndex
+    });
+  }
+
+  handleReset = () => {
+    this.setState({
+      snackCount: 0
+    });
+  }
+
   render() {
-    return <div className="App">Snack App goes here!</div>;
+    const { possibleSnacks, snackIndex, snackCount } = this.state;
+    
+    if(snackCount < 30) {
+      return (
+        <div className="App">
+          <h1>Give Baby Yoda some snack.</h1>
+          <img src={cookieGrogu} alt="baby yoda"/>
+          <div className="card">
+            <div>Baby Yoda has eaten <span className="snackCount">{snackCount}</span> snacks.</div>
+            <button onClick={this.changeSnack}>Change snack</button>
+            <button onClick={this.giveSnack}>Give snack</button>
+            <div>Current snack selected: <strong>{possibleSnacks[snackIndex]}</strong></div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <h1>Baby Yoda is full. Good job!</h1>
+          <img src={happyGrogu} alt="Happy Baby Yoda"/>
+          <div className="card">
+            <div>Come back another time.</div>
+            <button onClick={this.handleReset}>Start over</button>
+          </div>
+        </div>
+      )
+    }
   }
 }
 
